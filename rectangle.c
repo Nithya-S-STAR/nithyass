1fa28edbebe8f559fec426867fb8fd5ba8fa00a9
Nithya-S-STAR <72503421+Nithya-S-STAR@users.noreply.github.com>
2021-04-15 19:29:49 +0530

    Update rectangle.c

--- rectangle.c
+++ rectangle.c
@@ -1,87 +1,109 @@
 //WAP to compute area of rectangle
 #include<stdio.h>
 #include<math.h>
-struct points
+
+typedef struct
 {
-   float x;
-   float y;
-};
-typedef struct points point;
-struct rectangles
+	float x;
+	float y;
+}vertex;
+
+typedef struct
 {
-   float length;
-   float breadth;
-   float area;
-};
-typedef struct rectangles rectangle;
-int max_no_of_instances()
+	float length;
+	float breadth;
+	float area;
+	vertex v[3];
+}rectangle;
+
+int input_n()
 {
-   int max;
-   printf(“Enter the number of rectangles:\n”);
-   scanf(“%d”,&max);
-   return max;
+	int n;
+	printf("Enter number of test cases: ");
+	scanf("%d",&n);
+	return n;
 }
-void input( point p[3])
+
+rectangle input_rectangle()
 {
-   printf(“Enter the coordinates of points:\n”);
-   for(int i = 0 ; i<3; i++)
-   {
-       scanf(“%d %d” ,&p[i].x.&p[i].y);
-   }
+    rectangle r;
+	for(int i=0;i<3;i++)
+    {
+		scanf("%f%f",&r.v[i].x,&r.v[i].y);
+	}
+	return r;
 }
-void input_rectangle(int n , rectangle r[n])
+
+void input_n_rectangle(int n, rectangle r[n])
 {
-   point p[3];
-   for(int i=0;i<n;i++)
-  {
-     printf(“\nEnter the coordinates of rectangle %d”,i+1);
-     r[i] = input(p);
+	for(int i=0;i<n;i++)
+	{
+		r[i] = input_rectangle();
+	}
 }
-}
-void display(point p[3])
+
+float max(float side1, float side2, float side3)
 {
-   for(int i =0; i<3; i++)
-   {
-      printf(“(%.1f)(%.1f)\t”,p[i].x,p[i].y);
-    }
+ 	if(side1>side2 && side1>side3)
+		return side1;
+	else if(side2>side1 && side2>side3)
+		return side2;
+	else
+		return side3;
 }
-void display_rectangle(int n , rectangle r[n])
+
+float min(float side1, float side2, float side3)
 {
-   for(int i=0;i<n;i++)
-   {
-      printf(“\nThe coordinates for rectangle %d”,i+1);
-      display(r[i]);
-    }
+    	if(side1<side2 && side1<side3)
+		return side1;
+	else if(side2<side1 && side2<side3)
+		return side2;
+	else
+		return side3;
 }
-void compute_area( int n , rectangle r[n] )
+
+void compute_rectangle(rectangle *r)
 {
-    rectangle r[3];
-    float area;
-     for(int i =0;i<3;i++)
-     {
-        r[i].length = sqrt(pow(((p[1].x)-(p[0].x)),2)+(pow(((p[1].y)-(p[0].y)),2));
-        r[i].breadth = sqrt(pow(((p[2].x)-(p[1].x)),2)+(pow(((p[2].y-(p[1].y)),2));
-        r[i].area = r[i].length*r[i].breadth;
-       }
-} 
-void output(rectangle *r , point p[3])
-{
-    printf(“\nArea of rectangle with vertices (%.1f,%.1f)(%.1f,.%1f)(%.1f,.%1f) is %.1f”, p[0].x,p[0].y,p[1].x,p[1].y,p[2].x,p[2].y);
+	float side1,side2,side3,min_side,max_side,sum;
+	side1 = sqrt(pow((r->v[0].x-r->v[1].x),2)+pow((r->v[0].y-r->v[1].y),2));
+	side2 = sqrt(pow((r->v[2].x-r->v[1].x),2)+pow((r->v[2].y-r->v[1].y),2));
+	side3 = sqrt(pow((r->v[2].x-r->v[0].x),2)+pow((r->v[2].y-r->v[0].y),2));
+	min_side = min(side1,side2,side3);
+	max_side = max(side1,side2,side3);
+	sum = side1 + side2 + side3;
+	r->breadth = sum - min_side - max_side;
+    	r->length = min_side;
+	r->area = r->length * r->breadth;
 }
-void output_n_values(int n , rectangle r[n])
+
+void compute_n_rectangle(int n, rectangle r[n])
 {
-    for(int i=0; i<n;i++)
-     {
-         output(&r[i]);
-     }
+	for(int i=0;i<n;i++)
+	{
+		compute_rectangle(&r[i]);
+	}
 }
+
+void output_rectangle(rectangle r)
+{
+	printf("\nArea of rectangle with vertices (%.1f,%.1f), (%.1f,%.1f), (%.1f,%.1f) is %.1f",r.v[0].x,r.v[0].y,r.v[1].x,r.v[1].y,r.v[2].x,r.v[2].y,r.area);
+}
+
+void output_n_rectangle(int n, rectangle r[n])
+{
+	for(int i=0;i<n;i++)
+	{
+		output_rectangle(r[i]);
+	}
+}
+
 int main()
 {
-max = max_no_of_instances();
-rectangle r[max] ;
-input_rectangle(max,r);
-display_rectangle(max,r);
-compute_area(max,r);
-output_n_values(max,r);
-return 0;
+	int n;
+	n = input_n();
+	rectangle r[n];
+	input_n_rectangle(n,r);
+	compute_n_rectangle(n,r);
+	output_n_rectangle(n,r);
+	return 0;
 }
